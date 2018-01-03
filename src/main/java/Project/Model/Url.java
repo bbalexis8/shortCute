2a5db611b7886d95422e583ef310b46c57703fbd
bbalexis8 <alexismalabry@hotmail.fr>
2018-01-03 14:23:40 +0100

    working on project

--- src/main/java/Project/Model/Url.java
+++ src/main/java/Project/Model/Url.java
@@ -4,8 +4,12 @@ package Project.Model;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -17,39 +21,47 @@ import java.util.Date;
     @Table(name = "Url")
     public class Url implements Serializable, Validator {
 
-        private static final long serialVersionUID = 368875986784459499L;
+    private static final long serialVersionUID = 368875986784459499L;
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Column(name = "id", unique = true, nullable = false)
-        @Getter
-        @Setter
-        private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    @Getter
+    @Setter
+    private long id;
 
-        @Column(name="chaine")
-        @Getter @Setter private String chaine;
+    @Column(name = "chaine")
+    @Getter
+    @Setter
+    private String chaine;
 
-        @Column(name="code")
-        @Getter @Setter private String code;
+    @Column(name = "code")
+    @Getter
+    @Setter
+    private String code;
 
-        @Column(name = "createdAt")
-        @Getter @Setter private Date createdAt;
+    @Column(name = "createdAt")
+    @Getter
+    @Setter
+    private Date createdAt;
 
 
-        @ManyToOne(fetch = FetchType.LAZY)
-        @JoinColumn(name = "id_user", nullable = true)
-        @Getter @Setter private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_user", nullable = true)
+    @Getter
+    @Setter
+    private User user;
 
-        public Url() {
+    public Url() {
 
-        }
+    }
 
-        public Url(String chaine, User user) {
-            this.chaine = chaine;
-            this.user = user;
-            this.createdAt = new Date();
-            this.code = setCode();
-        }
+    public Url(String chaine, User user) {
+        this.chaine = chaine;
+        this.user = user;
+        this.createdAt = new Date();
+        this.code = setCode();
+    }
 
     public Url(String chaine) {
         this.chaine = chaine;
@@ -59,14 +71,12 @@ import java.util.Date;
 
     private String setCode() {
         Random boucle = new Random();
-        String code="";
-        for (int i = 0; i < 3; i++)
-        {
-            code +=(char) (boucle.nextInt(26 )+'a');
+        String code = "";
+        for (int i = 0; i < 3; i++) {
+            code += (char) (boucle.nextInt(26) + 'a');
 
         }
-        for (int i = 0; i < 3; i++)
-        {
+        for (int i = 0; i < 3; i++) {
             code += boucle.nextInt(10);
         }
         return code;
@@ -80,6 +90,31 @@ import java.util.Date;
 
     @Override
     public void validate(Object o, Errors errors) {
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "url", "NotEmpty.url.chaine");
 
+        // Validate an url
+        if (urlValidator(chaine))
+            System.out.print("The given URL " + chaine + " is valid");
+        else
+            System.out.print("The given URL " + chaine + " is not  valid");
     }
+
+
+    public static boolean urlValidator(String url)
+    {
+        /*validating url*/
+        try {
+            new URL(url).toURI();
+            return true;
+        }
+        catch (URISyntaxException exception) {
+            System.out.println("bad url");
+            return false;
+        }
+
+        catch (MalformedURLException exception) {
+            return false;
+        }
+    }
+
 }
